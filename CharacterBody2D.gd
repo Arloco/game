ac7badd8e6c609ceac027b8ba5ca296cbed6365d
Arlo <arlocomadley@gmail.com>
2024-02-20 10:07:04 -0800

    first commit

---
+++ CharacterBody2D.gd
@@ -0,0 +1,79 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+var shoot_right = false
+var shoot_left = false
+@export var mana = 100
+@export var regen_rate = 4
+@export var bullet_speed = 1000
+var flip = 1
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var object=preload("res://area_2d.tscn")
+
+
+
+	
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	$/root/World/CharacterBody2D/Camera2D/HScrollBar.value = mana
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	move_and_slide()
+	
+	if direction > 0:
+		flip = 1
+	if direction < 0:
+		flip = -1
+	
+	
+	var instance = object.instantiate()
+	
+	if mana < 100:
+		mana += regen_rate*delta
+
+		
+	
+	
+	if Input.is_action_just_pressed("shoot"):
+		if mana >= 10:
+			print("f")
+			add_child(instance)
+
+			if flip == 1:
+				shoot_right = true
+			if flip == -1:
+				shoot_left = true
+		
+			
+		
+	if shoot_right == true:
+		
+		instance.linear_velocity.x = bullet_speed
+		shoot_right = false
+		mana -= 10
+			
+	if shoot_left == true:
+		
+		instance.linear_velocity.x = -bullet_speed
+		shoot_left = false
+		mana -= 10
+
+
---
+++ Inside.gd
@@ -0,0 +1,22 @@
+extends Area2D
+var door_open = false
+
+
+func _process(delta):
+	print(door_open)
+	if door_open == true:
+		print("d") 
+		if Input.is_action_pressed("ui_select"):
+			get_tree().change_scene_to_file("res://outside.tscn")
+			print ("test2")
+	
+func _on_body_entered(body):
+	if body.name == "CharacterBody2D":
+		door_open = true
+		print ("test")
+
+
+func _on_body_exited(body):
+	if body.name == "CharacterBody2D":
+		door_open = false
+		print ("test3")
